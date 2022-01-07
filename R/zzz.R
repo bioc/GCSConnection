8a41ba174cd4886f9334bb2898e4457aa80ceae7
lshep <lori.shepherd@roswellpark.org>
2022-01-07 12:34:23 -0500

    Maintainer requested deprecation

--- R/zzz.R
+++ R/zzz.R
@@ -14,3 +14,9 @@ NULL
     pkg_namespace <- getNamespace(pkgname)
     C_package_onLoad(pkg_namespace)
 }
+.onAttach <- function(libname, pkgname) {
+    msg <- sprintf(
+        "Package '%s' is deprecated and will be removed from Bioconductor
+         version %s", pkgname, "3.16")
+    .Deprecated(msg=paste(strwrap(msg, exdent=2), collapse="\n"))
+}
